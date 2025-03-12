ede8dda6dd01eec5ca383f9c25bf57a9677ca29d
G <116308628+gashaw11@users.noreply.github.com>
2025-03-12 22:14:16 +0000

    save

--- myapp/views.py
+++ myapp/views.py
@@ -3,5 +3,5 @@ from .models import Product
 
 def hello_world(request):
     products = Product.objects.all()
-    return render(request, 'hello.html',{'products':products})
+    return render(request, 'base.html',{'products':products})
 
